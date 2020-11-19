25010e17c7078597b04928ef1e290dcbedd7f563
Ralph Landon <ralph7c2@gmail.com>
2020-11-19 09:42:18 -0800

    Add debugging

--- cache.sh
+++ cache.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 if [[ -d "$RENDER_ROOT"/public ]]; then
+  echo "Copying cached public dir"
   cp -rf "$RENDER_ROOT"/public public
+else
+  echo "No cached public dir found"
 fi
 
-gatsby build
+echo "Building"
+echo $(gatsby build)
 
+echo "Done, caching public dir"
 cp -rf public "$RENDER_ROOT"/public
