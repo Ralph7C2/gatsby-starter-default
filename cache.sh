8ccaa9c7b0f1e6f2139277d92d1c9f49987fc954
Ralph Landon <ralph7c2@gmail.com>
2020-11-19 10:12:07 -0800

    npm update

--- cache.sh
+++ cache.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-if [[ -d "$RENDER_ROOT"/public ]]; then
-  echo "Copying cached public dir"
-  cp -rf "$RENDER_ROOT"/public public
+build_with_cache() {
+  if [[ -d "$RENDER_ROOT"/public ]]; then
+    echo "Copying cached public dir"
+    cp -rf "$RENDER_ROOT"/public public
+  else
+    echo "No cached public dir found"
+  fi
+
+  echo "Building"
+
+  gatsby build
+
+  echo "Done, caching public dir"
+  cp -rf public "$RENDER_ROOT"/public
+}
+
+if [[ ""$RENDER" ]]; then
+  build_with_cache
 else
-  echo "No cached public dir found"
-fi
+  gatsby build
 
-echo "Building"
-
-gatsby build
-
-echo "Done, caching public dir"
-cp -rf public "$RENDER_ROOT"/public
